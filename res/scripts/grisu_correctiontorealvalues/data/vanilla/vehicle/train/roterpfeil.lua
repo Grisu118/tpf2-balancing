7eafedda09e6ff435c17efe53b7c31caf3d6acd6
Benjamin Leber <grisu118@grisu118.ch>
2019-12-21 15:00:05 +0100

    ✨ [vanilla/train] Add red arrow config

---
+++ res/scripts/grisu_correctiontorealvalues/data/vanilla/vehicle/train/roterpfeil.lua
@@ -0,0 +1,7 @@
+return {
+  -- TODO Should we fix the train driver? Both are visible when driving forward, none when driving backwards
+  loadConfigs = {
+    -- 70 seats, 30 standing
+    passengers = 80
+  }
+}
