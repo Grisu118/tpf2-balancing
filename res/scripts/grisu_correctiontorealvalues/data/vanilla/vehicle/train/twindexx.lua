dc8c619b8d6ce9ad8da4b11a27baf7abfa3d3697
SeamonTpF <smilodon@mail.ch>
2019-12-30 20:33:39 +0100

    Create twindexx.lua

---
+++ res/scripts/grisu_correctiontorealvalues/data/vanilla/vehicle/train/twindexx.lua
@@ -0,0 +1,26 @@
+return {
+  metadata = {
+    name = "Twindexx",
+    availability = {
+      yearFrom = 2014,
+    },
+    loadSpeed = 4,
+    railVehicle = {
+      engines = {
+        power = 1900,
+        tractiveEffort = 280,
+      },
+    },
+
+  },
+  loadSpeed = 4,  -- two wide doors
+  railVehicle = {
+    availability = {
+      yearFrom = 2018,
+    },
+    engines = {
+        power = 2812.5,
+        tractiveEffort = 180.75,
+    },
+  },
+}
