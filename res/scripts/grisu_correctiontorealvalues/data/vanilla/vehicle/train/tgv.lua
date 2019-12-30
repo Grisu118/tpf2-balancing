664fad0155716be23e6d44ece00a247e0ac28a33
SeamonTpF <59328227+SeamonTpF@users.noreply.github.com>
2019-12-30 17:07:09 +0100

    Create tgv.lua

---
+++ res/scripts/grisu_correctiontorealvalues/data/vanilla/vehicle/train/tgv.lua
@@ -0,0 +1,19 @@
+return {
+  metadata = {
+    name = "TGV",
+    loadSpeed = 1,
+    railVehicle = {
+      engines = {
+        power = 3225,
+        tractiveEffort = 350,
+      },
+    },
+  },
+  loadSpeed = 0,  -- no entrance for passengers
+  railVehicle = {
+    engines = {
+        power = 3225,
+        tractiveEffort = 175,
+    },
+  },
+}
