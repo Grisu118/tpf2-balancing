c4e162c3960aca8e72d22b2c8aa776602d3bcb9a
SeamonTpF <59328227+SeamonTpF@users.noreply.github.com>
2019-12-30 17:04:00 +0100

    Create ice1.lua

---
+++ res/scripts/grisu_correctiontorealvalues/data/vanilla/vehicle/train/ice1.lua
@@ -0,0 +1,18 @@
+return {
+  metadata = {
+    name = "ICE 1",
+    loadSpeed = 1,
+    railVehicle = {
+      engines = {
+        power = 2500,
+        tractiveEffort = 100,
+      },
+    },
+  },
+    loadSpeed = 0,  -- no entrance for passengers
+    railVehicle = {
+    engines = {
+        power = 4800,
+        tractiveEffort = 200,
+    },
+}
