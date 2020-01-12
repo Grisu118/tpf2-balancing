fb718f0ee93fa00751d5659503480fae9d182ef3
Benjamin Leber <grisu118@grisu118.ch>
2020-01-12 15:31:52 +0100

    ✨ [vanilla/tram] Add europe vanilla trams

---
+++ res/scripts/grisu_correctiontorealvalues/data/vanilla/vehicle/tram/atm_4000.lua
@@ -0,0 +1,22 @@
+return {
+  metadata = {
+    name = "ATM 4000",
+    loadSpeed = 5,
+    capacities = {
+      passengers = 104
+    },
+    availability = {
+      yearFrom = 1949,
+      yearTo = 2010,
+    },
+  },
+  capacities = {
+    -- Real: 45+115 Passagiere
+    passengers = 42 + 7
+  },
+  loadSpeed = 4.5,
+  availability = {
+    yearFrom = 1940,
+    yearTo = 1961,
+  },
+}
---
+++ res/scripts/grisu_correctiontorealvalues/data/vanilla/vehicle/tram/strassenbahn1850.lua
@@ -0,0 +1,18 @@
+return {
+  metadata = {
+    name = "Pferdebahn",
+    loadSpeed = 2,
+    capacities = {
+      passengers = 24
+    },
+    availability = {
+      yearFrom = 1850,
+      yearTo = 1905,
+    },
+  },
+  capacities = {
+    -- Real: 16+ Passagiere
+    passengers = 10 + 1
+  },
+  loadSpeed = 2,
+}
