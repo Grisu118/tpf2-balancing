b2799c61161cb07be1e9bb9832ca65b9ec100cb4
Benjamin Leber <grisu118@grisu118.ch>
2020-04-21 21:07:49 +0200

    ✨ [vanilla/ship] Add three more ships

---
+++ res/scripts/grisu_correctiontorealvalues/data/vanilla/vehicle/ship/herkules_xi.lua
@@ -0,0 +1,20 @@
+return {
+  metadata = {
+    name = "Herkules XI",
+    loadSpeed = 100,
+    capacities = {
+      _all = {
+        600,
+        600,
+      }
+    },
+    availability = {
+      yearFrom = 1990,
+      yearTo = 0,
+    }
+  },
+  capacities = {
+    _all = 600 -- 2 * 600 = 1200
+  },
+  loadSpeed = 100
+}
